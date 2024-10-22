876919a39958f2c89b16d39156939b420bc246f9
Tania Mathern <mathern@adobe.com>
2024-10-22 15:46:45 -0700

    Missing export

--- c2pa/__init__.py
+++ c2pa/__init__.py
@@ -11,7 +11,7 @@
 # specific language governing permissions and limitations under
 # each license.
 
-from .c2pa_api import Reader, Builder, create_signer, sign_ps256
+from .c2pa_api import Reader, Builder, create_signer, create_remote_signer, sign_ps256
 from .c2pa import Error, SigningAlg, CallbackSigner, sdk_version, version
 
-__all__ = ['Reader', 'Builder', 'create_signer', 'sign_ps256', 'Error', 'SigningAlg', 'sdk_version', 'version', 'CallbackSigner']
+__all__ = ['Reader', 'Builder', 'create_signer', 'sign_ps256', 'Error', 'SigningAlg', 'sdk_version', 'version', 'CallbackSigner', 'create_remote_signer']
