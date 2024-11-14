42fe14ea117d431e9e90b27b2ef0a91917588e5e
tmathern <60901087+tmathern@users.noreply.github.com>
2024-11-14 13:52:06 -0800

    Update __init__.py

--- c2pa/__init__.py
+++ c2pa/__init__.py
@@ -12,6 +12,7 @@
 # each license.
 
 from .c2pa_api import Reader, Builder, create_signer, create_remote_signer, sign_ps256
-from .c2pa import Error, SigningAlg, CallbackSigner, sdk_version, version, _UniffiConverterTypeSigningAlg, _UniffiRustBuffer
+from .c2pa import Error, SigningAlg, CallbackSigner, sdk_version, version
+from .c2pa.c2pa import _UniffiConverterTypeSigningAlg, _UniffiRustBuffer
 
 __all__ = ['Reader', 'Builder', 'CallbackSigner', 'create_signer', 'sign_ps256', 'Error', 'SigningAlg', 'sdk_version', 'version', 'create_remote_signer', '_UniffiConverterTypeSigningAlg', '_UniffiRustBuffer']
