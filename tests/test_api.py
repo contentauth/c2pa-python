1a681d4b74ca85dbcc3b3df4aa5c1708dc875b9e
Tania Mathern <mathern@adobe.com>
2025-01-30 12:00:22 -0800

    fix: Up versions

--- tests/test_api.py
+++ tests/test_api.py
@@ -63,7 +63,7 @@ ingredient_def = {
 
 class TestC2paSdk(unittest.TestCase):
     def test_version(self):
-        assert version() == "0.6.2"
+        assert version() == "0.6.3"
 
     def test_sdk_version(self):
         assert "c2pa-rs/" in sdk_version()
