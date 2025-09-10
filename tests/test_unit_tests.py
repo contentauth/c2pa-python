81c4afadbb434bfb7325be0cdb026f72aaca8405
tmathern <60901087+tmathern@users.noreply.github.com>
2025-09-10 12:44:33 -0700

    chore: Update c2pa-native-version to v0.63.0 (#160)
    * Update c2pa-native-version.txt
    
    * fix: Update test_unit_tests.py
    
    * fix: Update c2pa-native-version.txt
    
    * Update test_unit_tests.py

--- tests/test_unit_tests.py
+++ tests/test_unit_tests.py
@@ -40,7 +40,7 @@ ALTERNATIVE_INGREDIENT_TEST_FILE = os.path.join(FIXTURES_DIR, "cloud.jpg")
 
 class TestC2paSdk(unittest.TestCase):
     def test_sdk_version(self):
-        self.assertIn("0.61.0", sdk_version())
+        self.assertIn("0.63.0", sdk_version())
 
 
 class TestReader(unittest.TestCase):
