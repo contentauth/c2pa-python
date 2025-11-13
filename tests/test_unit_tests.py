fe6cb96a8ec1659cc692d2cf90cccf6310aea49b
tmathern <60901087+tmathern@users.noreply.github.com>
2025-11-13 10:14:10 -0800

    fix: Clean up logs from native lib

--- tests/test_unit_tests.py
+++ tests/test_unit_tests.py
@@ -67,7 +67,7 @@ def load_test_settings_json():
 
 class TestC2paSdk(unittest.TestCase):
     def test_sdk_version(self):
-        self.assertIn("0.71.1", sdk_version())
+        self.assertIn("0.71.2", sdk_version())
 
 
 class TestReader(unittest.TestCase):
