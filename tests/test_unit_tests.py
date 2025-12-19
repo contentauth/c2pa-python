ae5be2a1d02e042b259d2fbfbf08d6da3513b0f0
tmathern <60901087+tmathern@users.noreply.github.com>
2025-12-19 11:55:42 -0800

    fix: Clarify tests (#203)
    * fix: Initial example
    
    * fix: Switch to other runner
    
    * fix: Clarify other usages

--- tests/test_unit_tests.py
+++ tests/test_unit_tests.py
@@ -577,7 +577,8 @@ class TestReader(unittest.TestCase):
                     "data": {
                         "actions": [
                             {
-                                "action": "c2pa.opened"
+                                "action": "c2pa.created",
+                                "digitalSourceType": "http://cv.iptc.org/newscodes/digitalsourcetype/digitalCreation"
                             }
                         ]
                     }
@@ -745,8 +746,8 @@ class TestBuilderWithSigner(unittest.TestCase):
                     "data": {
                         "actions": [
                             {
-                                "action": "c2pa.opened"
-                                # Should have more parameters here, but omitted in tests
+                                "action": "c2pa.created",
+                                "digitalSourceType": "http://cv.iptc.org/newscodes/digitalsourcetype/digitalCreation"
                             }
                         ]
                     }
@@ -2227,8 +2228,9 @@ class TestBuilderWithSigner(unittest.TestCase):
                     "data": {
                         "actions": [
                             {
-                                "action": "c2pa.opened",
-                                "description": "Opened with Unicode: test"
+                                "action": "c2pa.created",
+                                "description": "Unicode: test",
+                                "digitalSourceType": "http://cv.iptc.org/newscodes/digitalsourcetype/digitalCreation"
                             }
                         ]
                     }
