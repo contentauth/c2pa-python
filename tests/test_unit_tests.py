f19695d731d55aac5731aef94130138bde609617
tmathern <60901087+tmathern@users.noreply.github.com>
2026-02-02 10:22:40 -0800

    chore: Bump c2pa-rs version to c2pa-v0.75.10 (#223)
    * Update c2pa-native-version.txt
    
    * Update test_unit_tests.py
    
    * Update test_unit_tests.py
    
    * Update c2pa-native-version.txt
    
    * Retrigger all tests at once

--- tests/test_unit_tests.py
+++ tests/test_unit_tests.py
@@ -67,7 +67,8 @@ def load_test_settings_json():
 
 class TestC2paSdk(unittest.TestCase):
     def test_sdk_version(self):
-        self.assertIn("0.75.8", sdk_version())
+        # This test verifies the native libraries used match the expected version
+        self.assertIn("0.75.10", sdk_version())
 
 
 class TestReader(unittest.TestCase):
